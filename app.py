f7d62f2e55ee4b2d9e0012973224c3dbb6114948
Chim chikwakwa <chimwec98@gmail.com>
2023-08-18 15:35:18 +0200

    this was good

--- app.py
+++ app.py
@@ -11,4 +11,12 @@ def index():
 #when i click the link in the browser it should take me to the route
 @app.route("/home")
 def home():
-    return render_template(home)
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    app.run()
