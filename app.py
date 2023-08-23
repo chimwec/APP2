1311c07d72ebb48ccda04af2fd334205a96af20e
Chim chikwakwa <chimwec98@gmail.com>
2023-08-23 21:39:47 +0200

    best one yet

--- app.py
+++ app.py
@@ -4,18 +4,22 @@ app = Flask(__name__)
 app.secret_key = "munthu koma uyu"
 
 
-@app.route("/hello")
+@app.route("/home")
 def index():
     return render_template("index.html")
 
 #when i click the link in the browser it should take me to the route
-@app.route("/home")
-def home():
-    return render_template("home.html")
-
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/game")
+def game():
+    return render_template("game.html")
 
 
 if __name__ == '__main__':
