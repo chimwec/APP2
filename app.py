14059798db15ba81c02299b6ad5b8b88d2ceb872
Chim Chikwakwa <chimwec98@gmail>
2023-11-23 07:19:43 +0100

    adding user passord and accounts

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route("/home")
@@ -27,6 +36,15 @@ def news():
 @app.route("/follow_us")
 def follow_us():
   return render_template("follow_us.html")
+
+# update User to inherit from UserMixin here:
+class User(UserMixin,db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(64), index=True, unique=True)
+
+  # add the email and password_hash attributes here:
+  email = db.Column(db.String(120), index = True, unique = True)
+  password_hash = db.Column(db.String(128))
 
 
 if __name__ == '__main__':
